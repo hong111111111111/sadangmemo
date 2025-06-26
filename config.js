9ee92737c2c74f95c9373a2b63427f62e8cb0a83
hong111111111111 <ixtys0704@gmail.com>
2025-06-26 11:31:00 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCJRrW7tyGi6y9JmBx6psT9CbNgjrQD9Tc",
+  authDomain: "sadang-memo-9db7a.firebaseapp.com",
+  projectId: "sadang-memo-9db7a",
+  storageBucket: "sadang-memo-9db7a.firebasestorage.app",
+  messagingSenderId: "637822482569",
+  appId: "1:637822482569:web:92ad3175232faf7a2e2e58",
+  measurementId: "G-H1LT2MR9CP"
+};
